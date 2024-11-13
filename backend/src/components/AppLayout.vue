8bc8dc68b4fe76567ab6f3e8cd92a23334e2b121
SFrancisc <szantofrancisc08@gmail.com>
2024-11-13 22:40:49 +0200

    bult-admin-panner-ui

--- backend/src/components/AppLayout.vue
+++ backend/src/components/AppLayout.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="min-h-full flex">
+    <div class="min-h-full bg-gray-100 flex">
       <!-- Sidebar -->
 
-      <Sidebar />
+      <Sidebar :class="{'-ml-[200px]': !sidebarOpened}"/>
       <!-- Sidebar-->
 
       <div class="flex-1">
-        <TopHeader></TopHeader>
+        <Navbar @toggle-sidebar="toggleSidebar"></Navbar>
         <!-- Header -->
-        <header class="h-8 shadow bg-white">
-          Header
-        </header>
+
         <!-- Content -->
-        <main>
-          <router-view></router-view>
+        <main class="p-6">
+            <div class="p-4 rounded bg-white">
+                <router-view></router-view>
+            </div>
         </main>
         <!-- Content -->
       </div>
@@ -21,12 +21,32 @@
   </template>
 
   <script setup>
+import {ref, onMounted, onUnmounted} from 'vue';
 import Sidebar from "./Sidebar.vue";
-import TopHeader from "./TopHeader.vue";
+import Navbar from "./Navbar.vue";
 
-  const { title } = defineProps({
+const { title } = defineProps({
     title: String,
-  });
+ });
+
+const sidebarOpened = ref(true);
+
+function toggleSidebar() {
+    sidebarOpened.value = !sidebarOpened.value
+}
+
+onMounted(() => {
+    handleSidebarOpened();
+    window.addEventListener('resize', handleSidebarOpened)
+})
+
+onUnmounted(() => {
+    window.removeEventListener('resize', handleSidebarOpened)
+})
+
+function handleSidebarOpened() {
+    sidebarOpened.value = window.outerWidth > 768;
+}
   </script>
 
   <style scoped>
--- backend/src/components/Sidebar.vue
+++ backend/src/components/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-[200px] bg-indigo-700 py-4 px-2">
+    <div class="w-[200px] transition-all bg-indigo-700 py-4 px-2">
         <router-link :to="{name: 'app.dashboard'}" class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
             <span class="mr-2 text-gray-300">
                 <HomeIcon class="w-5"/>
@@ -10,7 +10,7 @@
             </span>
         </router-link>
 
-        <router-link :to="{name: 'app.dashboard'}" class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+        <router-link :to="{name: 'app.products'}" class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
             <span class="mr-2 text-gray-300">
                 <ListBulletIcon class="w-5"/>
 
